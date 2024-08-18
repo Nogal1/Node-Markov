dc7ec49d86be6d21561a9ee41344cf4ac19743b7
Nogal1 <mcclendon.logan1234@gmail.com>
2024-08-18 12:04:33 -0700

    markov.js written

--- markov.js
+++ markov.js
@@ -17,13 +17,44 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    this.chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+      this.chains[word].push(nextWord);
+    }
   }
+
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    // Pick a random word to start with
+    let keys = Object.keys(this.chains);
+    let key = this._choice(keys);
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = this._choice(this.chains[key]);
+    }
+
+    return output.join(" ");
+  }
+
+  /** pick a random choice from an array */
+
+  _choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
   }
 }
+
+// Example usage
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm.makeText());
